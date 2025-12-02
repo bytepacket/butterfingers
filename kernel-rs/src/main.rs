4f38d31c68a841898b3a7afc7f7e7045ea3effc1
Arif Khan <153022036+bytepacket@users.noreply.github.com>
2025-12-02 00:42:28 +0000

    Added IO trait, as well as Tank impl and Drivetrain integration

--- kernel-rs/src/main.rs
+++ kernel-rs/src/main.rs
@@ -1,16 +1,30 @@
 #![no_main]
 
 use vexide::prelude::*;
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, rc::Rc, result};
 use evian::prelude::*;
-use evian::tracking::wheeled::TrackingWheel;
+use evian::drivetrain::model::Differential;
+use evian::tracking::shared_motors;
+use vexide::smart::motor::{BrakeMode, MotorType};
+use vexide::smart::PortError;
+
+pub trait IOFunc: Tank {
+    fn intake_store(&mut self) -> Result<(), Self::Error>;
+
+    fn outtake_top(&mut self) -> Result<(), Self::Error>;
+
+    fn outtake_middle(&mut self) -> Result<(), Self::Error>;
+
+    fn outtake_bottom(&mut self) -> Result<(), Self::Error>;
+
+    fn stop_intake(&mut self) -> Result<(), Self::Error>;
+}
 
 struct Robot<> {}
 
-
 struct Drivetrain<> {
-    left_motors: Rc<RefCell<[Motor; 3]>>,
-    right_motors: Rc<RefCell<[Motor; 3]>>,
+    left: MotorType,
+    right: MotorType,
 }
 
 struct IO<> {
@@ -19,14 +33,37 @@ struct IO<> {
     back_top: Motor,
 }
 
+pub trait TankExt: Tank {
+    fn brake_all(&mut self, mode: BrakeMode) -> Result<(), Self::Error>;
+}
+impl TankExt for Differential {
+    fn brake_all(&mut self, mode: BrakeMode) -> Result<(), Self::Error> {
+        let mut rtn = Ok(());
+
+        for motor in self.left.borrow_mut().as_mut() {
+            let result = motor.brake(mode);
+            if result.is_err() {
+                rtn = result;
+            }
+        }
+
+        for motor in self.right.borrow_mut().as_mut() {
+            let result = motor.brake(mode);
+            if result.is_err() {
+                rtn = result;
+            }
+        }
+
+        rtn
+    }
+}
+
 // Rc is necessary because we share motors between drivetrain and odometry, doesn't have Copy trait
 pub struct Odometry<> {
     left_motors: Rc<RefCell<[Motor; 3]>>,
     right_motors: Rc<RefCell<[Motor; 3]>>,
     imu: InertialSensor,
     color_sensor: OpticalSensor,
-    horizontal: TrackingWheel<AdiEncoder<360>>,
-    vertical: TrackingWheel<AdiEncoder<360>>,
 }
 
 impl Compete for Robot {
@@ -41,47 +78,38 @@ impl Compete for Robot {
 
 #[vexide::main]
 async fn main(peripherals: Peripherals) {
-
     let controller = peripherals.primary_controller;
 
     let screen = peripherals.display;
 
     let robot = Robot {};
 
-    let left_motors = Rc::new(RefCell::new([
+    let left_motors = shared_motors![
         Motor::new(peripherals.port_1, Gearset::Blue, Direction::Reverse),
         Motor::new(peripherals.port_2, Gearset::Blue, Direction::Reverse),
         Motor::new(peripherals.port_3, Gearset::Blue, Direction::Reverse),
-    ]));
+    ];
 
-    let right_motors = Rc::new(RefCell::new([
-        Motor::new(peripherals.port_4, Gearset::Blue, Direction::Reverse),
-        Motor::new(peripherals.port_8, Gearset::Blue, Direction::Reverse),
-        Motor::new(peripherals.port_10, Gearset::Blue, Direction::Reverse),
-    ]));
+    let right_motors = shared_motors![
+        Motor::new(peripherals.port_4, Gearset::Blue, Direction::Forward),
+        Motor::new(peripherals.port_8, Gearset::Blue, Direction::Forward),
+        Motor::new(peripherals.port_10, Gearset::Blue, Direction::Forward),
+    ];
 
-    let horizontal = AdiEncoder::new(peripherals.adi_a, peripherals.adi_b);
-    let vertical = AdiEncoder::new(peripherals.adi_g, peripherals.adi_h);
 
-    let horizontal_encoder = TrackingWheel::new(horizontal, 3.25, 0.0, None);
+    let mut dt = Differential::from_shared(left_motors.clone(), right_motors.clone());
 
-    let vertical_encoder = TrackingWheel::new(vertical, 3.25, 0.0, None);
+    let _ = dt.brake_all(BrakeMode::Brake);
 
     let mut imu = InertialSensor::new(peripherals.port_13);
     _ = imu.calibrate().await;
 
-    let drivetrain = Drivetrain {
-        left_motors: left_motors.clone(),
-        right_motors: right_motors.clone(),
-    };
 
     let odom = Odometry {
         left_motors: left_motors,
         right_motors: right_motors,
         imu: imu,
         color_sensor: OpticalSensor::new(peripherals.port_20),
-        horizontal: horizontal_encoder,
-        vertical: vertical_encoder,
     };
 
     let io = IO {
